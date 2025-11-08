ff50053c83f5c6c69f386b64e6bfccc00c6ef10b
penghui <penghui.zhang@foxmail.com>
2025-11-08 19:38:12 +0800

    feat: echo server v3
    supports multiple clients using thread pool with max capacity, thread
    s in poll will be reused to handle client requests. However, if client
    number exceeds thread pool capacity, client request are blocked like
    previous version.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,84 @@
 use std::{
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Arc, Mutex,
+    },
+    thread::JoinHandle,
 };
+
+pub struct Worker {
+    id: usize,
+    handle: JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let handle = std::thread::spawn(move || loop {
+            // lock is released immediately after let statement so other threads can acquire lock
+            // again when current job is running
+            let job = receiver.lock().unwrap().recv();
+
+            // using while let will hold lock until while let loop ends, which means current thread
+            // blocks other threads from acquiring lock when running job, so mutli-thread not working
+            match job {
+                Ok(job) => {
+                    job();
+                }
+                Err(_) => {
+                    eprintln!("Worker {id} disconnected, shuting down");
+                }
+            }
+        });
+        Worker { id, handle }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    sender: Option<Sender<Job>>,
+    workers: Vec<Worker>,
+}
+
+impl ThreadPool {
+    fn new(count: usize) -> ThreadPool {
+        assert!(count > 0);
+
+        let mut workers = Vec::with_capacity(count);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for i in 0..count {
+            let worker = Worker::new(i, Arc::clone(&receiver));
+            workers.push(worker)
+        }
+
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
+    }
+
+    fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in self.workers.drain(..) {
+            worker.handle.join().unwrap();
+        }
+    }
+}
 
 fn handle_connection(mut stream: TcpStream) {
     println!("New connection: {}", stream.peer_addr().unwrap());
@@ -28,16 +105,16 @@ fn handle_connection(mut stream: TcpStream) {
 }
 
 fn main() {
-    let listener = TcpListener::bind("localhost:0").unwrap();
+    let listener = TcpListener::bind("localhost:8000").unwrap();
     let addr = listener.local_addr().unwrap();
     println!("Server started on {}", addr);
+
+    let pool = ThreadPool::new(2);
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                // 创建一个线程处理一次请求
-                // let handle =
-                std::thread::spawn(|| {
+                pool.execute(|| {
                     handle_connection(stream);
                 });
             }
