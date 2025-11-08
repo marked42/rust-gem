8c644ed89f1fdc6b5fbf5996f86c7c1616426dd5
penghui <penghui.zhang@foxmail.com>
2025-11-08 17:46:59 +0800

    feat: echo server v2
    supports multi clients using new thread for each client, this method
    is inefficient and resource consuming

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,45 @@
 use std::{
     io::{BufRead, BufReader, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
+fn handle_connection(mut stream: TcpStream) {
+    println!("New connection: {}", stream.peer_addr().unwrap());
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+
+    loop {
+        let mut message = String::new();
+        match reader.read_line(&mut message) {
+            Ok(0) => {
+                println!("End");
+                break;
+            }
+            Ok(bytes_read) => {
+                println!("Received message {bytes_read} bytes : {message}");
+                let _ = stream.write_all(message.as_bytes());
+
+                message.clear();
+            }
+            Err(e) => {
+                eprintln!("Error: {e}")
+            }
+        }
+    }
+}
+
 fn main() {
-    let listener = TcpListener::bind("localhost:8000").unwrap();
+    let listener = TcpListener::bind("localhost:0").unwrap();
+    let addr = listener.local_addr().unwrap();
+    println!("Server started on {}", addr);
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                let mut reader = BufReader::new(stream.try_clone().unwrap());
-
-                loop {
-                    let mut message = String::new();
-                    match reader.read_line(&mut message) {
-                        Ok(0) => {
-                            println!("End");
-                            break;
-                        }
-                        Ok(bytes_read) => {
-                            println!("Received message {bytes_read} bytes : {message}");
-                            let _ = stream.write_all(message.as_bytes());
-
-                            message.clear();
-                        }
-                        Err(e) => {
-                            eprintln!("Error: {e}")
-                        }
-                    }
-                }
+            Ok(stream) => {
+                // 创建一个线程处理一次请求
+                // let handle =
+                std::thread::spawn(|| {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 eprintln!("Connection failed: {}", e);
