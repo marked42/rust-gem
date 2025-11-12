da8a03401b25048a6e1371f065d2dc2a3a0912a0
penghui <penghui.zhang@foxmail.com>
2025-11-12 19:51:29 +0800

    feat: delay future example

---
+++ my-redis/examples/delay.rs
@@ -0,0 +1,32 @@
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.when {
+            println!("Hello world!");
+            Poll::Ready("done")
+        } else {
+            // TODO: this wakes future immediate
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let when = Instant::now() + Duration::from_secs(3);
+    let future = Delay { when };
+
+    let out = future.await;
+
+    assert_eq!(out, "done");
+}
