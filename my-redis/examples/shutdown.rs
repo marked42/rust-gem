7acd5fc58732cbb869b1e7b1a589a7d031799dd1
penghui <penghui.zhang@foxmail.com>
2025-11-14 18:47:40 +0800

    feat: graceful shutdown example

---
+++ my-redis/examples/shutdown.rs
@@ -0,0 +1,24 @@
+use std::time::Duration;
+use tokio::sync::mpsc;
+use tokio::time;
+
+#[tokio::main]
+async fn main() {
+    let (sender, mut receiver) = mpsc::channel(1);
+
+    for i in 0..10 {
+        tokio::spawn(some_operation(i, sender.clone()));
+    }
+
+    // drop sender
+    drop(sender);
+
+    let r = receiver.recv().await;
+
+    println!("app shutdown {:?}", r);
+}
+
+async fn some_operation(i: u64, sender: mpsc::Sender<u32>) {
+    time::sleep(Duration::from_millis(100 * i)).await;
+    println!("Task {} shutting down", i);
+}
