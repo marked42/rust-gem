ab24ce838b293a527996d44cdcdbba0ff2183478
penghui <penghui.zhang@foxmail.com>
2025-11-17 14:57:32 +0800

    refactor: png file read/write
    1. move file read/write methods to png impl
    2. replace &PathBuf with AsRef<Path> as path type which can support multiple
    types (&str, &Path, &Path, &PathBuf)

--- pngme/src/commands.rs
+++ pngme/src/commands.rs
@@ -1,6 +1,3 @@
-use std::fs::OpenOptions;
-use std::io::Write;
-use std::path::PathBuf;
 use std::str::FromStr;
 
 use crate::Result;
@@ -9,38 +6,23 @@ use crate::chunk::Chunk;
 use crate::chunk_type::ChunkType;
 use crate::png::Png;
 
-fn dump_file(png: &Png, path: &PathBuf) -> Result<()> {
-    let data = png.as_bytes();
-
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .truncate(true) // 清空文件
-        .open(path)?;
-
-    file.write_all(&data)?;
-    file.sync_all()?;
-
-    Ok(())
-}
-
 /// Encodes a message into a PNG file and saves the result
 pub fn encode(args: EncodeArgs) -> Result<()> {
     let path = &args.path;
-    let mut png = Png::try_from_path(path)?;
+    let mut png = Png::from_file(path)?;
 
     let chunk_type = ChunkType::from_str(args.chunk_type.as_str())?;
     let chunk = Chunk::new(chunk_type, args.message.into_bytes())?;
     png.append_chunk(chunk);
 
     let output_file = args.output_file.unwrap_or(args.path);
-    dump_file(&png, &output_file)
+    png.save_to_file(output_file)
 }
 
 /// Searches for a message hidden in a PNG file and prints the message if one is found
 pub fn decode(args: DecodeArgs) -> Result<()> {
     let path = &args.path;
-    let png = Png::try_from_path(path)?;
+    let png = Png::from_file(path)?;
 
     let chunk = png.chunk_by_type(&args.chunk_type);
 
@@ -56,7 +38,7 @@ pub fn decode(args: DecodeArgs) -> Result<()> {
 /// Removes a chunk from a PNG file and saves the result
 pub fn remove(args: RemoveArgs) -> Result<()> {
     let path = &args.path;
-    let mut png = Png::try_from_path(path)?;
+    let mut png = Png::from_file(path)?;
 
     let chunk = png.remove_first_chunk(&args.chunk_type);
     if chunk.is_ok() {
@@ -66,13 +48,13 @@ pub fn remove(args: RemoveArgs) -> Result<()> {
     }
 
     let output_file = args.output_file.unwrap_or(args.path);
-    dump_file(&png, &output_file)
+    png.save_to_file(output_file)
 }
 
 /// Prints all of the chunks in a PNG file
 pub fn print_chunks(args: PrintArgs) -> Result<()> {
     let path = &args.path;
-    let png = Png::try_from_path(path)?;
+    let png = Png::from_file(path)?;
 
     // 检查是否有 chunks
     if png.chunks().is_empty() {
