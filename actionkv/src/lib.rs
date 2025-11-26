879e355ede15ba5d502b48338923117e7cb00e9f
penghui <penghui.zhang@foxmail.com>
2025-11-26 20:52:30 +0800

    feat: mini kv store

---
+++ actionkv/src/lib.rs
@@ -0,0 +1,180 @@
+use std::{
+    collections::HashMap,
+    fs::{File, OpenOptions},
+    io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write},
+    path::Path,
+};
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use crc::{CRC_32_ISO_HDLC, Crc};
+use serde::{Deserialize, Serialize};
+
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+static CRC_IEEE: Crc<u32> = Crc::<u32>::new(&CRC_32_ISO_HDLC);
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
+}
+
+#[derive(Debug)]
+pub struct ActionKV {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
+
+impl ActionKV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new().read(true).write(true).create(true).append(true).open(path)?;
+        let index = HashMap::new();
+
+        Ok(ActionKV { f, index })
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut f = BufReader::new(&mut self.f);
+        loop {
+            let position = f.seek(SeekFrom::Current(0))?;
+            let maybe_kv = ActionKV::process_record(&mut f);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::UnexpectedEof => break,
+                    _ => return Err(err),
+                },
+            };
+
+            self.index.insert(kv.key, position);
+        }
+        Ok(())
+    }
+
+    fn process_record<R: Read>(f: &mut R) -> io::Result<KeyValuePair> {
+        let saved_checksum = f.read_u32::<LittleEndian>()?;
+        let key_len = f.read_u32::<LittleEndian>()?;
+        let val_len = f.read_u32::<LittleEndian>()?;
+        let data_len = key_len + val_len;
+
+        let mut data = ByteString::with_capacity(data_len as usize);
+
+        {
+            // by_ref avoids .take taking ownership of f
+            // vec![0; data_len as usize] + read_exact, double write with array initialization
+            f.by_ref().take(data_len as u64).read_to_end(&mut data)?;
+        }
+
+        let check_sum = CRC_IEEE.checksum(&data);
+        if check_sum != saved_checksum {
+            panic!(
+                "data corruption encountered ({:08x} != {:08x})",
+                check_sum, saved_checksum
+            )
+        }
+
+        let value = data.split_off(key_len as usize);
+        let key = data;
+
+        Ok(KeyValuePair { key, value })
+    }
+
+    pub fn get(&self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        let Some(position) = self.index.get(key) else {
+            return Ok(None);
+        };
+        let kv = self.get_at(*position)?;
+
+        Ok(Some(kv.value))
+    }
+
+    pub fn find_from_start(
+        &mut self,
+        target: &ByteString,
+    ) -> io::Result<Option<(u64, ByteString)>> {
+        self.seek_to_start()?;
+        self.find_from_current(target)
+    }
+
+    pub fn seek_to_start(&mut self) -> io::Result<()> {
+        self.f.seek(SeekFrom::Start(0))?;
+        Ok(())
+    }
+
+    pub fn find_from_current(&mut self, target: &ByteStr) -> io::Result<Option<(u64, ByteString)>> {
+        let mut f = BufReader::new(&mut self.f);
+
+        let mut found: Option<(u64, ByteString)> = None;
+
+        loop {
+            let position = f.seek(SeekFrom::Start(0))?;
+            let maybe_kv = ActionKV::process_record(&mut f);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::UnexpectedEof => break,
+                    _ => return Err(err),
+                },
+            };
+
+            if kv.key.as_slice() == target {
+                found = Some((position, kv.value));
+                break;
+            }
+        }
+
+        Ok(found)
+    }
+
+    pub fn get_at(&self, position: u64) -> io::Result<KeyValuePair> {
+        let mut f = BufReader::new(&self.f);
+        f.seek(SeekFrom::Start(position))?;
+        let kv = ActionKV::process_record(&mut f)?;
+
+        Ok(kv)
+    }
+
+    pub fn insert(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        let position = self.insert_but_ignore_index(key, value)?;
+
+        self.index.insert(key.to_vec(), position);
+
+        Ok(())
+    }
+
+    // insert append new key value at the end of file
+    pub fn insert_but_ignore_index(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<u64> {
+        // FIXME: don't create BufWrite every time
+        let mut f = BufWriter::new(&mut self.f);
+
+        let position = f.seek(SeekFrom::End(0))?;
+
+        let key_len = key.len();
+        let val_len = value.len();
+        let mut tmp = ByteString::with_capacity(key_len + val_len);
+        tmp.extend_from_slice(key);
+        tmp.extend_from_slice(value);
+
+        let checksum = CRC_IEEE.checksum(&tmp);
+
+        f.write_u32::<LittleEndian>(checksum)?;
+        f.write_u32::<LittleEndian>(key_len as u32)?;
+        f.write_u32::<LittleEndian>(val_len as u32)?;
+        f.write_all(&tmp)?;
+
+        Ok(position)
+    }
+
+    #[inline]
+    pub fn update(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        self.insert(key, value)
+    }
+
+    #[inline]
+    pub fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        self.insert(key, b"")
+    }
+}
